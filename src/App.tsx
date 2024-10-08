ccb5a88d58add7a7e5cffc2be9aa65cd26a35ff4
JeanCarlos <jmatochepascual@gmail.com>
2024-10-08 11:28:17 -0500

    feat: :sparkles: Crea el componente Actividad para representar visulamente cada actividad agregada
    Se implementa la creación del componente Actividad que representa visualmente cada actividad agregar y se listan las actividades agregadas al estado

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useReducer } from "react"
 import Form from "./component/Form"
 import Header from "./component/Header"
 import { actividadReducer, estadoInicial } from "./reducer/actividadReducer"
+import Actividad from "./component/Actividad";
 
 
 function App() {
@@ -14,6 +15,27 @@ function App() {
       <Form
         dispatch={dispatch}
       />
+
+      <section className="py-20">
+        <div className="w-11/12 max-w-5xl mx-auto">
+          <h2 className="mb-12 text-3xl font-bold text-center text-gray-600">Comida y Actividades</h2>
+
+          {
+            estado.actividades.length === 0
+              ? <p className="text-center text-lg text-gray-500 mt-4">No hay actividades</p>
+              : <>
+                {
+                  estado.actividades.map(actividad =>
+                    <Actividad
+                      key={actividad.id}
+                      actividad={actividad}
+                    />
+                  )
+                }
+              </>
+          }
+        </div>
+      </section>
     </>
   )
 }
