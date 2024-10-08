84023724053e528dc7d8b255164b0e7b02b84df2
JeanCarlos <jmatochepascual@gmail.com>
2024-10-07 21:17:49 -0500

    feat: :sparkles: Crea el componente Header
    Crea el componente Header con solo cambios visuales, sin lógica agregada

---
+++ src/component/Header.tsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <header className="bg-lime-600">
+      <div className="w-11/12 max-w-5xl mx-auto p-8">
+        <h1 className="text-4xl text-center font-bold text-white uppercase">Contador de Calorias</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
