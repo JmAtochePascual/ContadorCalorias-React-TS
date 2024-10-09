22741b352ba48e510f9b0e9447d724632ab9780e
JeanCarlos <jmatochepascual@gmail.com>
2024-10-09 05:55:31 -0500

    feat: :heavy_plus_sign: Agreha la dependencia de heroIcons para uso de iconos
    Instala heroicons para el uso de iconos en los botones

--- src/component/ActivityList.tsx
+++ src/component/ActivityList.tsx
@@ -1,5 +1,6 @@
 import { categories } from "../data/data"
 import { TActivity } from "../types"
+import { PencilSquareIcon } from "@heroicons/react/24/outline"
 
 type ActivityListProps = {
   activities: TActivity[]
@@ -13,27 +14,29 @@ const ActivityList = ({ activities }: ActivityListProps) => {
     <>
       {
         activities.length === 0
-          ? <p className="text-center text-4xl font-extrabold text-gray-600">No hay actividades registradas...</p>
+          ? <p className="text-center text-gray-600">No hay actividades registradas...</p>
           : activities.map(activity =>
             <div
               key={activity.id}
-              className="mb-4 p-4 flex justify-between items-center shadow-md rounded-md bg-white ">
+              className="mb-4 p-4 flex flex-col gap-4 shadow-md rounded-md bg-white md:flex-row md:justify-between md:items-center">
 
               <div>
-                <p className={`mb-1 px-4 py-1 inline-block font-bold text-white ${activity.category === 1 ? "bg-lime-500" : "bg-orange-500"}`}>
+                <p className={`mb-1 px-4 py-1 font-bold text-center text-white ${activity.category === 1 ? "bg-lime-500" : "bg-orange-500"} md:inline-block`}>
                   {categoryName(activity.category)}
                 </p >
-                <p className="text-lg font-bold uppercase text-gray-700"> {activity.name} </p>
-                <p className="text-4xl font-extrabold text-lime-500"> {activity.calorie} Calorias</p>
+                <p className="text-center text-lg font-bold uppercase text-gray-700 md:text-start"> {activity.name} </p>
+                <p className="text-center text-4xl font-extrabold text-lime-500 md:text-start" > {activity.calorie} Calorias</p>
               </div>
 
-              <div>
-                <button className="mr-2 p-2 bg-sky-600 text-white rounded-md">Editar</button>
-                <button className="p-2 bg-rose-500 text-white rounded-md">Eliminar</button>
+              <div className="flex gap-4">
+                <button className="w-full p-2 flex gap-1 bg-sky-600 text-white rounded-md">
+                  Editar
+                  <PencilSquareIcon className="w-6 h-6 inline-block" />
+                </button>
+
+                <button className="w-full p-2 block bg-rose-500 text-white rounded-md">Eliminar</button>
               </div>
             </div>
-
-
           )
       }
     </>
